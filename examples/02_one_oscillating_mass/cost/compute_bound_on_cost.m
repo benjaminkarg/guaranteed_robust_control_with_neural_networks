324182d382a33d368681c1d94a096b4e8d0a2483
Benjamin Karg <benjamin.karg@tu-dortmund.de>
2022-02-09 17:51:58 +0100

    bound on cost for one oscillating mass

---
+++ examples/02_one_oscillating_mass/cost/compute_bound_on_cost.m
@@ -0,0 +1,30 @@
+clear all;
+close all;
+addpath('../../auxiliary_funs/');
+
+
+%% Params
+u_lb = -5.0; % lower bound of control input
+u_ub =  5.0; % upper bound of control input
+
+
+%% Load the neural network
+load('./../data/nn_controller.mat');
+network = make_network_input_admissible(network, u_lb, u_ub);
+
+
+%% Load system data
+load('./../data/system_and_problem_matrices.mat');
+
+
+%% Load minimum RPI
+load('./../data/iterative_min_RPI.mat');
+min_RPI = Polyhedron(H{end, 1}, h{end, 1});
+
+
+%% compute worst-case cost for min RPI
+max_cost = max_stage_cost(network, min_RPI, Q, R, A, B, E);
+
+
+%% save
+save('./../data/worst_case_cost_min_RPI.mat', 'max_cost');
