876be11e37f6dbec7ec42f3e502130d38f8d7f37
Benjamin Karg <benjamin.karg@tu-dortmund.de>
2022-02-09 17:55:12 +0100

    Update for data_generation in the closed loop

---
+++ examples/02_one_oscillating_mass/deep_learning/gen_data_for_closed_loop.py
@@ -0,0 +1,37 @@
+import numpy as np
+import sys
+sys.path.append('./../../../auxiliary_funs')
+from aux_funs import *
+import tensorflow as tf
+
+sys.path.append('../multi_stage_mpc')
+from template_model import *
+from template_simulator import *
+import scipy.io as sio
+
+
+""" Params """
+n_sim = 100
+n_steps = 150
+u_lb = np.array([[-5.0]])
+u_ub = np.array([[ 5.0]])
+x_lb = np.array([[-4.0], [-2.0]])
+x_ub = np.array([[ 4.0], [ 2.0]])
+savename = '../data/closed_loop_sim.mat'
+
+
+""" Load system data """
+data = sio.loadmat('./../data/system_and_problem_matrices.mat')
+H_x = np.array(data['H_x'], dtype = 'float')
+h_x = np.array(data['h_x'], dtype = 'float')
+
+
+""" Get configured do-mpc modules """
+model = template_model()
+mpc = tf.keras.models.load_model('../data/nn_controller.h5')
+simulator = template_simulator(model)
+estimator = do_mpc.estimator.StateFeedback(model)
+
+
+""" Generate data """
+gen_data_closed_loop(mpc, u_lb, u_ub, x_lb, x_ub, H_x, h_x, simulator, estimator, n_sim, n_steps, savename)
